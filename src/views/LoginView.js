703110dd00ce296f20a603a46359dfc1f40b5222
Roman <zummo3@gmail.com>
2017-01-03 01:25:51 +0300

    Add auth0

--- src/views/LoginView.js
+++ src/views/LoginView.js
@@ -7,12 +7,28 @@ import {
 import {
   Button
 } from 'react-native-elements'
+import Auth0Lock from 'react-native-lock'
 
 export default class SuperChat extends Component {
+  constructor (props) {
+    super(props)
+    this.onLoginClick = this.onLoginClick.bind(this)
+  }
+
+  onLoginClick () {
+    const lock = new Auth0Lock({
+      clientId: 'detKg5ugNQdbnno1V84YS3RPLja9REkT',
+      domain: 'superchat.eu.auth0.com'
+    })
+    lock.show({}, (err, profile, token) => {
+      console.log(err, profile, token)
+    })
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Button title='Perform login' onPress={() => {}} />
+        <Button title='Perform login' onPress={this.onLoginClick} />
       </View>
     );
   }
